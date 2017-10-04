c26d3d253ca257991e811d97b9361215819a67be
Judzhin Miles <info@msbios.com>
2017-10-04 17:00:10 +0300

    Lazy Composer Commiter

---
+++ config/module.config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+
+namespace MSBios\CPanel;
+
+return [
+
+    'service_manager' => [
+        'factories' => [
+
+        ]
+    ],
+
+    Module::class => [
+    ],
+];
---
+++ src/Module.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+
+namespace MSBios\Imagine;
+
+use MSBios\ModuleInterface;
+use Zend\EventManager\EventInterface;
+use Zend\EventManager\LazyListenerAggregate;
+use Zend\Loader\AutoloaderFactory;
+use Zend\Loader\StandardAutoloader;
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\Mvc\ApplicationInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class Module
+ * @package MSBios\CPanel
+ */
+class Module implements
+    ModuleInterface,
+    AutoloaderProviderInterface
+{
+    const VERSION = '0.0.1';
+
+    /**
+     * @return mixed
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+
+    /**
+     * Return an array for passing to Zend\Loader\AutoloaderFactory.
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            AutoloaderFactory::STANDARD_AUTOLOADER => [
+                StandardAutoloader::LOAD_NS => [
+                    __NAMESPACE__ => __DIR__,
+                ],
+            ],
+        ];
+    }
+}
